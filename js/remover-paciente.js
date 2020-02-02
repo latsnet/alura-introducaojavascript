457b047cafbab91de0ea1a0b2135b5d3df865ce9
Luiz Augusto Tavares da Silveira Mazzi <latsnet@gmail.com>
2020-02-02 14:49:33 -0300

    Efeito fadeOut na remoção dos pacientes

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,5 +1,8 @@
 var pacientes = document.querySelector("#tabela-pacientes");
 
 pacientes.addEventListener("dblclick", function(e) {
-    e.target.parentNode.remove();
+    e.target.parentNode.classList.add("fadeOut");
+    setTimeout(function() {
+        e.target.parentNode.remove();
+    }, 500);
 });
